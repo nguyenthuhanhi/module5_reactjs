f10f5b87f6dee9fe3c96eed639d3bbd8dcc6bffa
nguyenthuhanhi <nnhi85788@gmail.com>
2023-09-08 19:08:43 +0700

    first commit

---
+++ fibonacci-sum/fibonacci.js
@@ -0,0 +1,11 @@
+let fibonacci = (n) => {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    sum += fibonacci(i);
+}
+console.log(sum);
