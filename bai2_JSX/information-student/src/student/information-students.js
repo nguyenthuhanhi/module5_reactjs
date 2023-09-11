686afe5101f3a69024673e41ee6c5c1b5c01bf93
nguyenthuhanhi <nnhi85788@gmail.com>
2023-09-11 19:59:07 +0700

    11/09/2023

---
+++ bai2_JSX/information-student/src/student/information-students.js
@@ -0,0 +1,56 @@
+
+import './information-student.css';
+function Student() {
+    return(
+        <div>
+            <h1>Information Students</h1>
+            <ul>
+                {students.map(student => (
+                    <tr>
+                        <td>{student.company}</td>
+                        <td>{student.contact}</td>
+                        <td>{student.country}</td>
+                    </tr>
+                ))}
+            </ul>
+        </div>
+    );
+}
+const students = [
+    {
+        company: 'Alfreds Futterkiste',
+        contact: 'Maria Anders',
+        country: 'Germany'
+    },
+    {
+        company: 'Centro comercial Moctezuma',
+        contact: 'Francisco Chang',
+        country: 'Mexico'
+    },
+    {
+        company: 'Ernst Handel',
+        contact: 'Roland Mendel',
+        country: 'Austria'
+    },
+    {
+        company: 'Island Trading',
+        contact: 'Helen Bennett',
+        country: 'UK'
+    },
+    {
+        company: 'Laughing Bacchus Winecellars',
+        contact: 'Yoshi Tannamuri',
+        country: 'Canada'
+    },
+    {
+        company: 'Magazzini Alimentari Riuniti',
+        contact: 'Giovanni Rovelli',
+        country: 'Italy'
+    }
+];
+export default Student;
+
+
+
+
+
